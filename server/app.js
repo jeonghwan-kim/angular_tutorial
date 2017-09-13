9de4451143c653d9cc188e975373db6193e4deb9
김정환 <ej88ej@gmail.com>
2017-09-13 21:28:45 +0900

    [backend] rewrite es6  (arrow function, const)

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 // part 1
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const path = require('path');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var apiRouter = express.Router();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const apiRouter = express.Router();
 
 app.use(apiRouter);
 //body parser
@@ -13,17 +13,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', express.static(path.join(__dirname, '../client')));
 
-var test = fs.readFileSync('data/todos.json','utf8');
-var todos = JSON.parse(test);
+const test = fs.readFileSync('data/todos.json','utf8');
+let todos = JSON.parse(test);
 
-app.get('/api/todos', function(req,res){
+app.get('/api/todos', (req,res) => res.json(todos));
 
-  res.json(todos);
-
-});
-apiRouter.delete('/api/todos/:id', (req,res)=>{
-
-  var deletedTodoIdx;
+apiRouter.delete('/api/todos/:id', (req,res) => {
+  let deletedTodoIdx;
   for(i=0;i<todos.length;i++){
     if(todos[i].id == req.params.id)
     deletedTodoIdx = i;
@@ -33,23 +29,16 @@ apiRouter.delete('/api/todos/:id', (req,res)=>{
   console.log("index of it is: "+deletedTodoIdx);
   todos.splice(deletedTodoIdx, 1);
 
-  var json = JSON.stringify(todos);
+  const json = JSON.stringify(todos);
   fs.writeFile('data/todos.json', json, 'utf8');
   res.json(todos);
-
 });
 
-apiRouter.delete('/api/todos/', (req,res)=>{
-
-  // var deletedTodoIdx;
-  // for(i=0;i<todos.length;i++){
-  //   if(todos[i].id == req.params.id)
-  //   deletedTodoIdx = i;
-  // }
+apiRouter.delete('/api/todos/', (req,res) => {
   console.log(todos);
-  var old_todos = todos;
+  const old_todos = todos;
   todos = [];
-  for(var i in old_todos){
+  for(let i in old_todos){
     val  = old_todos[i];
     if(!val.completed){
       //do something
@@ -59,11 +48,11 @@ apiRouter.delete('/api/todos/', (req,res)=>{
   }
   console.log(todos);
   // angular.copy(incompleteTodos, storage.todos);
-  var json = JSON.stringify(todos);
+  const json = JSON.stringify(todos);
   fs.writeFile('data/todos.json', json, 'utf8');
   res.json(todos);
-
 });
+
 apiRouter.put('/api/todos/:id', (req,res)=>{
   console.log("id of the object that will have changed is "+req.params.id);
 
@@ -80,6 +69,7 @@ apiRouter.put('/api/todos/:id', (req,res)=>{
   res.json(todos);
 
 });
+
 apiRouter.put('/api/todos/:id/:title', (req,res)=>{
   console.log("id of the object that will have changed is "+req.params.id);
 
@@ -97,39 +87,35 @@ apiRouter.put('/api/todos/:id/:title', (req,res)=>{
 
 });
 
-app.post('/api/todos', function(req,res){
+app.post('/api/todos', (req,res) => {
 
-  var idOfLastItem = todos.length - 1;
+  const idOfLastItem = todos.length - 1;
   // console.log(todos[idOfLastItem]);
   // console.log(req.body.title);
   if(!req.body.title){
 
     return res.status(400).send('None shall pass');
   }
-  var newId = !todos.length ?
+  const newId = !todos.length ?
   1: todos[idOfLastItem].id+1;
 
-  var newTodo = {
+  const newTodo = {
     "id":newId,
     "title":req.body.title,
     "completed":false
   };
   todos.push(newTodo);
-  var json = JSON.stringify(todos);
+  const json = JSON.stringify(todos);
   fs.writeFile('data/todos.json', json, 'utf8');
-  // todos.push(newTodo);
-  // console.log(newTodo);
   res.json(todos);
-
 });
 
 // part 2
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
   res.sendfile('index.html')
 });
 
 // part 3
 app.listen(3000, function () {
-
   console.log('Example app listening on port 3000!');
 });
